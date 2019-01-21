c6e6af2cb5bc1645abcb9bbfbd760bf782d1fd08
lingjie zhang <zhanglingjie16303@gmail.com>
2019-01-21 15:31:16 -0500

    test get locations

--- proj3/src/main/java/MapServer.java
+++ proj3/src/main/java/MapServer.java
@@ -285,7 +285,7 @@ public class MapServer {
      * cleaned <code>prefix</code>.
      */
     public static List<String> getLocationsByPrefix(String prefix) {
-        return null;
+        return graph.getLocationsByPrefix(prefix);
     }
 
     /**
@@ -301,7 +301,19 @@ public class MapServer {
      * "id" : Number, The id of the node. <br>
      */
     public static List<Map<String, Object>> getLocations(String locationName) {
-        return null;
+        List<Map<String, Object>> locationList = new LinkedList<>();
+
+        List<Long> nodes = graph.getLocations(locationName);
+        for (long id : nodes) {
+            Map<String, Object> node = new HashMap<>();
+            node.put("id", id);
+            node.put("name", locationName);
+            node.put("lon", graph.locationLon(id));
+            node.put("lat", graph.locationLat(id));
+        }
+
+        return locationList;
+
     }
 
     /**
